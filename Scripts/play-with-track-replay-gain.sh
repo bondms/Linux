3fb125f670a69bc1120602bc47f4d04e3a33b7d5
Mark Bond <34947848+bondms@users.noreply.github.com>
2019-10-30 18:52:24 +0000

    Fix for locating helper script

--- Scripts/play-with-track-replay-gain.sh
+++ Scripts/play-with-track-replay-gain.sh
@@ -6,7 +6,10 @@ set -o pipefail
 [[ $# -eq 1 ]] || exit $?
 [[ -f "$1" ]] || exit $?
 
+HERE=$(readlink -e "$(dirname "$0")")
+[[ -d "${HERE}" ]] || exit $?
+
 # Automatic replay gain in SoX doesn't appear to be supported
 # for mp3 files, so disable it (for all files) and manually
 # apply ReplayGain.
-play "$1" --replay-gain off gain $(track-replay-gain.sh "$1") channels 1
+play "$1" --replay-gain off gain $("${HERE}/track-replay-gain.sh" "$1") channels 1
