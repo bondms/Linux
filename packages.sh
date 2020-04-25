b182285d27102a52f8fe1354b4d1ae71acac55cb
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-04-25 19:22:21 +0100

    meld package

--- packages.sh
+++ packages.sh
@@ -9,3 +9,4 @@ sudo apt install geany || exit $?
 sudo apt install synaptic || exit $?
 sudo apt install chromium-browser || exit $?
 sudo apt install git || exit $?
+sudo apt install meld || exit $?
