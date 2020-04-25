035e47d585821f0d36b8c4a2026b53a46c176d41
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-04-25 19:07:59 +0100

    Packages script

---
+++ packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+sudo apt update || exit $?
+
+sudo apt install geany || exit $?
+sudo apt install synaptic || exit $?
+sudo apt install chromium-browser || exit $?
+sudo apt install git || exit $?
