574258b2e6deb468382ad90cae02fc6a65e1702c
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-05-08 09:47:59 +0100

    Sort git settings

--- Git/settings.sh
+++ Git/settings.sh
@@ -3,4 +3,4 @@
 HERE="$(readlink -f "$(dirname "$0")")"
 [[ -d "${HERE}" ]] || exit $?
 
-git config --list --global > "${HERE}/global_settings.txt" || exit $?
+git config --list --global | sort > "${HERE}/global_settings.txt" || exit $?
