0234483bff7b08cdac82c161f8660af9b59e9ac3
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-05-14 23:30:46 +0100

    Update track-replay-gain.sh for rgain3, replacing broken bs1770gain

--- Scripts/track-replay-gain.sh
+++ Scripts/track-replay-gain.sh
@@ -6,6 +6,6 @@ set -o pipefail
 [[ $# -eq 1 ]] || exit $?
 [[ -f "$1" ]] || exit $?
 
-bs1770gain --list "${1}" 2>&1 |
-    grep --only-matching --perl-regex --ignore-case "(?<=REPLAYGAIN_TRACK_GAIN: ).*" |
+replaygain --show "${1}" 2>&1 |
+    grep --only-matching --perl-regex --ignore-case "(?<=Track gain ).*" |
     grep --only-matching --perl-regex ".*(?= dB)" || exit $?
