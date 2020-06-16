9a703edca100e813fa01edfcfb1f5f19a9705406
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-06-16 20:11:16 +0100

    Fix up replay gain for media PC

--- Scripts/track-replay-gain.sh
+++ Scripts/track-replay-gain.sh
@@ -6,6 +6,9 @@ set -o pipefail
 [[ $# -eq 1 ]] || exit $?
 [[ -f "$1" ]] || exit $?
 
-replaygain --show "${1}" 2>&1 |
+HERE=$(readlink -e "$(dirname "$0")")
+[[ -d "${HERE}" ]] || exit $?
+
+"${HERE}/replaygain" --show "${1}" 2>&1 |
     grep --only-matching --perl-regex --ignore-case "(?<=Track gain ).*" |
     grep --only-matching --perl-regex ".*(?= dB)" || exit $?
