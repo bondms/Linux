78a81d29e7986830b19e0581d058f0f890cdedd9
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-10-08 21:33:26 +0100

    Linting

--- Scripts/base16to36.py
+++ Scripts/base16to36.py
@@ -6,16 +6,16 @@ import sys
 def base36encode(number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
     """Converts an integer to a base36 string."""
     if not isinstance(number, int):
-        raise TypeError('number must be an integer')
+        raise TypeError("Number must be an integer")
 
     if number >= 0 and number <= 9:
         return alphabet[number]
 
-    base36 = ''
-    sign = ''
+    base36 = ""
+    sign = ""
 
     if number < 0:
-        sign = '-'
+        sign = "-"
         number = -number
 
     while number != 0:
