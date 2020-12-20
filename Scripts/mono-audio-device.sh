4ea3b08766cd36615e14d7eb1a5b5bf48571882c
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-12-20 16:24:46 +0000

    Helper scripts from mono-mapped audio output

---
+++ Scripts/mono-audio-device.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+[[ $# -eq 1 ]] || exit 1
+
+pacmd unload-module module-remap-sink || true
+pacmd load-module module-remap-sink sink_name=mono master="$1" channels=2 channel_map=mono,mono || exit $?
