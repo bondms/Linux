f54aec604b8ddd87610a36a0c0224d91229d0e73
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-08-21 16:57:12 +0100

    Fetch all pages of products

--- Scripts/OctopusEnergyApi/commands.sh
+++ Scripts/OctopusEnergyApi/commands.sh
@@ -23,7 +23,12 @@ DATE_STR=$(date +%Y-%m-%d)
 DATA_DIR_PATH="${HERE}/data/${DATE_STR}"
 mkdir -- "${DATA_DIR_PATH}" || exit $?
 
-curl -u "${API_KEY}:" "https://api.octopus.energy/v1/products/" > "${DATA_DIR_PATH}/products.json" || exit $?
+curl -u "${API_KEY}:" "https://api.octopus.energy/v1/products/" > "${DATA_DIR_PATH}/products_001.json" || exit $?
+for PAGE in {002..002}
+do
+    curl -u "${API_KEY}:" "https://api.octopus.energy/v1/products/?page=${PAGE}" > "${DATA_DIR_PATH}/products_${PAGE}.json" || exit $?
+done
+
 curl -u "${API_KEY}:" "https://api.octopus.energy/v1/products/${ELECTRICITY_PRODUCT}/" > "${DATA_DIR_PATH}/electricity_rates.json" || exit $?
 curl -u "${API_KEY}:" "https://api.octopus.energy/v1/industry/grid-supply-points/" > "${DATA_DIR_PATH}/gsps.json" || exit $?
 curl -u "${API_KEY}:" "https://api.octopus.energy/v1/industry/grid-supply-points/?postcode=${POSTCODE}" > "${DATA_DIR_PATH}/gsp.json" || exit $?
@@ -32,7 +37,6 @@ curl -u "${API_KEY}:" "https://api.octopus.energy/v1/products/${ELECTRICITY_PROD
 
 curl -u "${API_KEY}:" "https://api.octopus.energy/v1/electricity-meter-points/${ELECTRICITY_MPAN}/meters/${ELECTRICITY_SERIAL_NUMBER}/consumption/" > "${DATA_DIR_PATH}/electricity_001.json" || exit $?
 curl -u "${API_KEY}:" "https://api.octopus.energy/v1/gas-meter-points/${GAS_MPRN}/meters/${GAS_SERIAL_NUMBER}/consumption/" > "${DATA_DIR_PATH}/gas_001.json" || exit $?
-
 for PAGE in {002..999}
 do
     curl -u "${API_KEY}:" "https://api.octopus.energy/v1/electricity-meter-points/${ELECTRICITY_MPAN}/meters/${ELECTRICITY_SERIAL_NUMBER}/consumption/?page=${PAGE}" > "${DATA_DIR_PATH}/electricity_${PAGE}.json" || exit $?
