d378676604b4bd746bcb37358445b9bb3a03b0f2
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-10-24 19:31:35 +0100

    Fix bash syntax

--- Media-PC/sync.sh
+++ Media-PC/sync.sh
@@ -6,7 +6,7 @@ set -o pipefail
 HERE="$(readlink -e "$(dirname "$0")")"
 [[ -d "${HERE}" ]] || exit $?
 
-for subdir in "Music", "Pictures", "Playlists", "Podcasts"
+for subdir in "Music" "Pictures" "Playlists" "Podcasts"
 do
     rsync --archive --human-readable --itemize-changes --checksum -- "${HOME}/${subdir}/." "/media/bondms/rootfs/home/pi/${subdir}/." || exit $?
 done
