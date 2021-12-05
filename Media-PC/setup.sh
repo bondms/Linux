d122758075f145e7fc05b33af04ac06c1be13ec8
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-12-05 18:29:43 +0000

    Schedule recording for time-shifted playback

--- Media-PC/setup.sh
+++ Media-PC/setup.sh
@@ -56,6 +56,11 @@ fi
 [[ -h "${HERE}/../../rgain/scripts/rgain3" ]] ||
     ln --symbolic --verbose -- "../rgain3" "${HERE}/../../rgain/scripts/." || exit $?
 
+# Configure recording for time-shifted playback of Radio Varna
 mkdir --verbose --parents -- "${HOME}/Recordings" || exit $?
+crontab - << EOF
+# Record Radio Varna from 10:20 to 13:00 (2h40m) on Sundays.
+20 10 * * sun sox --clobber --type mp3 http://broadcast.masters.bg:8000/live "${HOME}\Recordings\radio-varna.mp3" trim 0 2:40:00"
+EOF
 
 echo "*** SUCCESS ***"
