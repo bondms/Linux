3e50196a3db5be91bfd064b2a085099abeffb00f
Mark Bond <34947848+bondms@users.noreply.github.com>
2024-11-30 19:01:46 +0000

    Backup

--- Scripts/audio-mono-device.sh
+++ Scripts/audio-mono-device.sh
@@ -4,3 +4,4 @@
 
 pactl unload-module module-remap-sink || true
 pactl load-module module-remap-sink sink_name=mono master="$1" channels=2 channel_map=mono,mono || exit 1
+pactl set-default-sink mono || exit 1
