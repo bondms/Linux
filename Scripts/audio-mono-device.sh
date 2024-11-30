ff0977a02e9849023759b9762e4420446d7dc8c9
Mark Bond <34947848+bondms@users.noreply.github.com>
2024-11-30 20:31:21 +0000

    Simplify script and make compatible with Pi

--- Scripts/audio-mono-device.sh
+++ Scripts/audio-mono-device.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -x
+
+set -e
+set -o pipefail
+set -u
+
 [[ $# -eq 1 ]] || exit 1
 
-pactl unload-module module-remap-sink || true
-pactl load-module module-remap-sink sink_name=mono master="$1" channels=2 channel_map=mono,mono || exit 1
+pactl unload-module module-remap-sink || exit 1
+pactl load-module module-remap-sink sink_name=mono master="$1" channels=1 master_channel_map=mono channel_map=mono remix=no || exit 1
 pactl set-default-sink mono || exit 1
