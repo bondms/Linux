90e44197130ec06ea5a89bf23853f1f310d1614c
Mark Bond <34947848+bondms@users.noreply.github.com>
2024-12-01 00:24:56 +0000

    Working on mono script

--- Scripts/audio-mono-device.sh
+++ Scripts/audio-mono-device.sh
@@ -12,6 +12,6 @@ echo "Device: ${DEVICE}"
 pactl set-default-sink "${DEVICE}" || exit 1
 
 pactl unload-module module-remap-sink || exit 1
-pactl load-module module-remap-sink sink_name=mono master="${DEVICE}" channels=1 master_channel_map=mono channel_map=mono remix=no || exit 1
+pactl load-module module-remap-sink sink_name=mono master="${DEVICE}" channels=2 channel_map=mono,mono || exit 1
 
 pactl set-default-sink mono || exit 1
