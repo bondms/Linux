47e03a4611cf086a41557aed493c92489588583b
Mark Bond <34947848+bondms@users.noreply.github.com>
2024-12-01 00:13:37 +0000

    Working on mono script

--- Scripts/audio-mono-device.sh
+++ Scripts/audio-mono-device.sh
@@ -3,8 +3,15 @@
 set -eux
 set -o pipefail
 
-[[ $# -eq 1 ]] || exit 1
+pactl unload-module module-stream-restore || exit 1
+# pactl load-module module-stream-restore restore_device=false || exit 1
+
+DEVICE="$(pactl list short sinks | grep -F stereo | cut -f 2)"
+echo "Device: ${DEVICE}"
+
+pactl set-default-sink "${DEVICE}" || exit 1
 
 pactl unload-module module-remap-sink || exit 1
-pactl load-module module-remap-sink sink_name=mono master="$1" channels=1 master_channel_map=mono channel_map=mono remix=no || exit 1
+pactl load-module module-remap-sink sink_name=mono master="${DEVICE}" channels=1 master_channel_map=mono channel_map=mono remix=no || exit 1
+
 pactl set-default-sink mono || exit 1
