f0e1a67598e3cf3a29dc670379ebf551b69efa88
Mark Bond <34947848+bondms@users.noreply.github.com>
2025-10-21 18:43:12 +0100

    Script to sync Google docs

---
+++ Scripts/fetch-googledocs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+SOURCE="remote:Docs"
+TARGET_DIR="${HOME}/Documents/GoogleDocs"
+LOGFILE="${HOME}/Backup/BackupLogs/rsync-googledocs.log"
+TIMESTAMP_PATH="${HOME}/Backup/BackupLogs/rsync-googledocs-timestamp.txt"
+
+rclone \
+    --links \
+    --verbose \
+    --human-readable \
+    --checksum \
+    sync \
+    "${SOURCE}/" "${TARGET_DIR}/" 2>&1 | tee "${LOGFILE}" || exit 1
+date --utc --iso-8601=seconds > "${TIMESTAMP_PATH}" || exit 1
