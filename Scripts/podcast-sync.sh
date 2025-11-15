b34f9aae345dadd6e5880560af2999b6a6b03431
Mark Bond <34947848+bondms@users.noreply.github.com>
2025-11-15 00:55:28 +0000

    Rehosting SN (#10)

---
+++ Scripts/podcast-sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+HERE="$(readlink -e "$(dirname "${BASH_SOURCE[0]}")")"
+[[ -d "$HERE" ]] || exit 1
+
+AUTH=$(gpg --decrypt -- "${HERE}/podcast-sync-secrets.sh.gpg" |
+  while read -r line
+  do
+    if [[ "AUTH=" == "${line:0:5}" ]]
+    then
+      echo "${line:5}"
+    fi
+  done) || exit 1
+
+[[ -n "${AUTH}" ]] || exit 1
+
+SOURCE="https://twit.memberfulcontent.com/rss/9054?auth=${AUTH}"
+TARGET="remote:Podcasts/sn.rss"
+
+curl "${SOURCE}" | rclone rcat "${TARGET}" || exit 1
