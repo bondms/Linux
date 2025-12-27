3faa15ed88ef5484b4c21d06a8c180abe9abd641
Mark Bond <34947848+bondms@users.noreply.github.com>
2025-12-27 09:47:32 +0000

    Fix mono HDMI audio

--- Scripts/audio-mono-device.sh
+++ Scripts/audio-mono-device.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# Sink names identified with `pactl list short sinks`.
+
 set -eux
 set -o pipefail
 
 pactl unload-module module-stream-restore || exit 1
-# pactl load-module module-stream-restore restore_device=false || exit 1
+pactl load-module module-stream-restore restore_device=false || exit 1
 
-DEVICE="$(pactl list short sinks | grep -F stereo | cut -f 2)"
-echo "Device: ${DEVICE}"
+HDMI_DEVICE="$(pactl list sinks short | cut -f 2 | grep -F hdmi-stereo)" || exit 1
+[[ -n "${HDMI_DEVICE}" ]] || exit 1
 
-pactl set-default-sink "${DEVICE}" || exit 1
+pactl set-default-sink "${HDMI_DEVICE}" || exit 1
 
 pactl unload-module module-remap-sink || exit 1
-pactl load-module module-remap-sink sink_name=mono master="${DEVICE}" channels=2 channel_map=mono,mono || exit 1
+pactl load-module module-remap-sink sink_name=hdmi_mono master="${HDMI_DEVICE}" channels=1 channel_map=mono || exit 1
 
-pactl set-default-sink mono || exit 1
+pactl set-default-sink hdmi_mono || exit 1
+
+echo "Sinks:"
+pactl list short sinks || exit 1
+
+echo "Sink inputs:"
+pactl list short sink-inputs || exit 1
+
+echo "Test with \`play-left-right-test\` or \"https://www.youtube.com/watch?v=6TWJaFD6R2s\"" || exit 1
