00c56f8e2b81f95bc950031a354b9921d8e2783f
Mark Bond <34947848+bondms@users.noreply.github.com>
2026-05-02 08:58:27 +0100

    Backup

--- Media-PC/sync.sh
+++ Media-PC/sync.sh
@@ -8,6 +8,17 @@ HERE="$(readlink --canonicalize-existing "$(dirname "${BASH_SOURCE[0]}")")"
 
 [[ -d "/media/bondms/rootfs/home/pi" ]] || exit 1
 
+rsync \
+    --archive \
+    --verbose \
+    --human-readable \
+    --itemize-changes \
+    --checksum \
+    --delete --delete-excluded \
+    --exclude ".git/" \
+    -- \
+    "${HERE}/../." "/media/bondms/rootfs/home/pi/Linux/." || exit 1
+
 for subdir in "Music" "Pictures" "Playlists" "Podcasts" "Videos"
 do
     mkdir --parents -- "/media/bondms/rootfs/home/pi/${subdir}" || exit 1
@@ -17,7 +28,7 @@ do
         --human-readable \
         --itemize-changes \
         --checksum \
-        --delete \
+        --delete --delete-excluded \
         -- \
         "${HOME}/${subdir}/." "/media/bondms/rootfs/home/pi/${subdir}/." || exit 1
 done
